190682a35ce197f8fc0583d64dceb5011953f34d
claratesmon <claraperezmontes@gmail.com>
2023-11-22 21:18:03 +0100

    nav bar and toggle menu

--- src/components/ShoesCarr.jsx
+++ src/components/ShoesCarr.jsx
@@ -3,8 +3,7 @@ import './shoescarr.css'
 const ShoesCarr = () => {
     return(
         <div>
-            <h2>teste</h2>
-            <h3>teste</h3>
+            
         </div>
 
     )
--- src/components/SlideButtons.jsx
+++ src/components/SlideButtons.jsx
@@ -1,11 +1,15 @@
 import './slidebuttons.css'
+/* import 'react-slideshow-image/dist/style.css'
+import React from 'react'
+import { Slide, Slides } from 'react-slideshow-image' */
 
 const SlideButtons = () => {
-    return(
-        <div>
+    return (
+        
+            <div>
 
-        </div>
-
+            </div>
+        
     )
 }
 export default SlideButtons;
