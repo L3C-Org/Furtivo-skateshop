b97bc0c76e2e7e886cbe0410686f4f1157d2f745
Mohamed Taha <Taha_Acme@hotmail.com>
2023-11-22 16:59:32 +0100

    blog entry

--- src/components/BlogCarr.jsx
+++ src/components/BlogCarr.jsx
@@ -2,6 +2,12 @@ import './blogcarr.css'
 const BlogCarr= () => {
     return(
         <div>
+            <h1>LATEST BLOG ENTRIES</h1>
+            <div>
+                <image></image>
+                <h6></h6>
+            </div>
+            
 
         </div>
 
